61401ec53871a54e22644adfc59d718ef86b893f
堤智春 <tsutsumitomoharu@tsutsumichiharus-MacBook-Pro.local>
2023-09-09 12:01:50 -0700

    spring executed

--- index.java
+++ index.java
@@ -1,11 +1,32 @@
-// This is a simple Java program.
-// FileName : "HelloWorld.java".
+// import java.io.IOException;
+// import java.io.OutputStream;
+// import java.net.InetSocketAddress;
+// import com.sun.net.httpserver.HttpExchange;
+// import com.sun.net.httpserver.HttpHandler;
+// import com.sun.net.httpserver.HttpServer;
 
-class HelloWorld {
-	// Your program begins with a call to main().
-	// Prints "Hello, World" to the terminal window.
-	public static void main(String args[])
-	{
-		System.out.println("Hello, World");
-	}
-}
+// public class SimpleServer {
+
+//     public static void main(String[] args) throws Exception {
+//         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+//         server.createContext("/", new RootHandler());
+//         server.setExecutor(null); // creates a default executor
+//         System.out.println("Server started and listening on port 8080");
+//         server.start();
+//     }
+
+//     static class RootHandler implements HttpHandler {
+//         @Override
+//         public void handle(HttpExchange exchange) throws IOException {
+//             String response = "Hello! You've connected to the server.";
+//             byte[] responseBytes = response.getBytes();
+            
+//             // Send headers (HTTP/1.0 200 OK)
+//             exchange.sendResponseHeaders(200, responseBytes.length);
+            
+//             OutputStream os = exchange.getResponseBody();
+//             os.write(responseBytes);
+//             os.close();
+//         }
+//     }
+// }
