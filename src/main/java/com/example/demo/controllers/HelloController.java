f13a9d0aae6cd25bfb712e0c892f9a0e557f877f
堤智春 <ttomoharu201@gmail.com>
2023-09-15 12:13:50 -0700

    starter-web

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.RestController;
