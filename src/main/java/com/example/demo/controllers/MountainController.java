efb17768ee954478174c83ab5ba7f64bd923d681
堤智春 <ttomoharu201@gmail.com>
2023-09-23 22:11:39 -0700

    listing mountains

--- src/main/java/com/example/demo/controllers/MountainController.java
+++ src/main/java/com/example/demo/controllers/MountainController.java
@@ -1,48 +1,74 @@
-package com.example.demo.controllers;
+// package com.example.demo.controllers;
 
-// import org.springframework.web.bind.annotation.RestController;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-// import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.bind.annotation.GetMapping;
-
-
-// @Controller
-// public class MountainController {
-
-//     @GetMapping("/index")
-//     public String index(Model model) {
-//         model.addAttribute("name", "John");
-//         return "index";  // This refers to home.html template
-//     }
-// }
-
-// package com.example.demo.controller;
-
+// // import org.springframework.web.bind.annotation.RestController;
 // import org.springframework.stereotype.Controller;
 // import org.springframework.ui.Model;
 // import org.springframework.web.bind.annotation.GetMapping;
 
-// @Controller
-// public class HomeController {
+// // import org.springframework.web.bind.annotation.RestController;
+// // import org.springframework.web.bind.annotation.GetMapping;
 
-//     @GetMapping("/home")
+
+// // @Controller
+// // public class MountainController {
+
+// //     @GetMapping("/index")
+// //     public String index(Model model) {
+// //         model.addAttribute("name", "John");
+// //         return "index";  // This refers to home.html template
+// //     }
+// // }
+
+// // package com.example.demo.controller;
+
+// // import org.springframework.stereotype.Controller;
+// // import org.springframework.ui.Model;
+// // import org.springframework.web.bind.annotation.GetMapping;
+
+// // @Controller
+// // public class HomeController {
+
+// //     @GetMapping("/home")
+// //     public String home(Model model) {
+// //         model.addAttribute("name", "John");
+// //         return "home";  // This refers to home.html template
+// //     }
+// // }
+
+// @Controller
+// public class MountainController {
+
+//     @GetMapping("/")
 //     public String home(Model model) {
 //         model.addAttribute("name", "John");
 //         return "home";  // This refers to home.html template
 //     }
 // }
 
+
+
+import com.example.demo.models.Mountain;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class MountainController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("name", "John");
+        // Create a mock list of Mountain instances
+        List<Mountain> mountains = Arrays.asList(
+                new Mountain(1, "MountainA", "PrefectureA", true, 12.345f, 54.321f),
+                new Mountain(2, "MountainB", "PrefectureB", false, 23.456f, 65.432f),
+                new Mountain(3, "MountainC", "PrefectureC", true, 34.567f, 76.543f)
+        );
+
+        // Add the list to the model
+        model.addAttribute("mountains", mountains);
+
         return "home";  // This refers to home.html template
     }
 }
-
-
