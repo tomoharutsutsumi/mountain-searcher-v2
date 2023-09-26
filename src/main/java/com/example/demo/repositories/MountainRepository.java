3afa14726f5da9d3f1ac9cd7c9a0b8b93eb74f2d
堤智春 <ttomoharu201@gmail.com>
2023-09-26 08:36:37 -0700

    fixing db

--- src/main/java/com/example/demo/repositories/MountainRepository.java
+++ src/main/java/com/example/demo/repositories/MountainRepository.java
@@ -4,4 +4,5 @@ import com.example.demo.models.Mountain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MountainRepository extends JpaRepository<Mountain, Integer> {
+    
 }
