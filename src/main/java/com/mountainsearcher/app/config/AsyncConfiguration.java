e49ef5a1bbc0e324ef7b0fb94b31cbe7b98b193f
堤智春 <ttomoharu201@gmail.com>
2023-10-23 15:21:28 -0700

    resttemplate

--- src/main/java/com/mountainsearcher/app/config/AsyncConfiguration.java
+++ src/main/java/com/mountainsearcher/app/config/AsyncConfiguration.java
@@ -7,26 +7,36 @@ public class WeatherService {
 
     private final RestTemplate restTemplate;
 
-    public WeatherService() {
-        this.restTemplate = new RestTemplate();
-    }
+    // public WeatherService() {
+    //     this.restTemplate = new RestTemplate();
+    // }
 
-    public List<WeatherData> fetchWeatherForMountains(List<Mountain> mountains) {
-        List<CompletableFuture<WeatherData>> futures = mountains.stream()
-            .map(mountain -> CompletableFuture.supplyAsync(() -> fetchWeather(mountain)))
-            .collect(Collectors.toList());
+    // public List<WeatherData> fetchWeatherForMountains(List<Mountain> mountains) {
+    //     List<CompletableFuture<WeatherData>> futures = mountains.stream()
+    //         .map(mountain -> CompletableFuture.supplyAsync(() -> fetchWeather(mountain)))
+    //         .collect(Collectors.toList());
 
-        return futures.stream()
-            .map(CompletableFuture::join)
-            .collect(Collectors.toList());
-    }
+    //     return futures.stream()
+    //         .map(CompletableFuture::join)
+    //         .collect(Collectors.toList());
+    // }
 
-    private WeatherData fetchWeather(Mountain mountain) {
-        String apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=" + mountain.getLatitude() 
+
+    RestTemplate restTemplate = new RestTemplate();
+    String apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=" + mountain.getLatitude() 
             + "&longitude=" + mountain.getLongitude()
             + "&daily=weathercode&timezone=auto";
-        return restTemplate.getForObject(apiUrl, WeatherData.class);
-    }
+    String result = restTemplate.getForObject(uri, String.class); //define object
+    // {"latitude":52.52,"longitude":13.419998,"generationtime_ms":0.00095367431640625,"utc_offset_seconds":0,"timezone":"GMT","timezone_abbreviation":"GMT","elevation":38.0}
+
+    System.out.println(result);
+
+    // private WeatherData fetchWeather(Mountain mountain) {
+    //     String apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=" + mountain.getLatitude() 
+    //         + "&longitude=" + mountain.getLongitude()
+    //         + "&daily=weathercode&timezone=auto";
+    //     return restTemplate.getForObject(apiUrl, WeatherData.class);
+    // }
 }
 
 //  caching should be used as well.
