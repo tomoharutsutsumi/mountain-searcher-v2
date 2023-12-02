dd0ef6283e9045cbc7db05165a9d8c319d258f45
堤智春 <ttomoharu201@gmail.com>
2023-12-02 09:00:34 -0800

    definfing weather code

--- src/main/java/com/mountainsearcher/app/models/WeatherResponse.java
+++ src/main/java/com/mountainsearcher/app/models/WeatherResponse.java
@@ -37,5 +37,33 @@ public class WeatherResponse {
     public List<Integer> getWeatherCode() {
         return daily.weathercode;
     }
+
+    private static final Map<Integer, String> WEATHER_CODES = Map.of(
+        0, "clear sky"
+        1, "Mainly clear"
+        2, "partly cloudy"
+        3, "overcast"
+        45, "Fog"
+        48, "depositing rime fog"
+        51, "Drizzle: Light"
+        53, "Drizzle: moderate"
+        55, "Drizzle: dense intensity"
+        56, "Freezing Drizzle: Light"
+        57, "Freezing Drizzle: dense intensity"
+        61, "Rain: Slight"
+        63, "Rain: moderate"
+        65, "Rain: heavy intensity"
+        66, "Freezing Rain: Light"
+        67, "Freezing Rain: heavy intensity"
+        71, "Snow fall: Slight"
+        73, "Snow fall: moderate"
+        75, "Snow fall: heavy intensity"
+
+77	Snow grains
+80, 81, 82	Rain showers: Slight, moderate, and violent
+85, 86	Snow showers slight and heavy
+95 	Thunderstorm: Slight or moderate
+96, 99 	Thunderstorm with slight and heavy hail
+    );
     
 }
