a77d558e6eb1443e261ed36938b4bf0e7c1d5e35
堤智春 <ttomoharu201@gmail.com>
2023-12-04 12:18:05 -0800

    weathermountain table

---
+++ src/main/java/com/mountainsearcher/app/models/WeatherMountain.java
@@ -0,0 +1,14 @@
+package com.mountainsearcher.app.models;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class WeatherMountain {
+    private int mountainId;
+    private int weatherCode;
+    private int day;
+}
+
